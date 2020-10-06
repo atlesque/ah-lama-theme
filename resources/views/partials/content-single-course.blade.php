67635ac47b71b2447edeae093fbdcdfd94fca74e
Alexander Van Maele <alexander@atlesque.com>
2020-10-06 22:36:57 +0200

    Add password protection for course overview pages

--- resources/views/partials/content-single-course.blade.php
+++ resources/views/partials/content-single-course.blade.php
@@ -1,7 +1,16 @@
 <article @php post_class() @endphp>
-    <div class="entry-content">
-        @php the_content() @endphp
-    </div>
+
+    @php global $post; @endphp
+    
+    @if(post_password_required( $post ) === true)
+        <h1>Protected content</h1>
+        {!! get_the_password_form() !!}
+    @else
+        <div class="entry-content">
+            @php the_content() @endphp
+        </div>
+    @endif
+    
     <footer>
         {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
     </footer>
